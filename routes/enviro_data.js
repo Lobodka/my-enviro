0a951de8ba5df7127605efe5aaf30bd2e94529f6
Leo Salzmann <leo.salzmann@proton.me>
2022-11-03 17:03:01 +0100

    last THUR commit

--- routes/enviro_data.js
+++ routes/enviro_data.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require("../model/helper"); // added during scaffolding
 
+
 /* GET all data */
 // not used in app, just wrote to test it
 router.get('/', function(req, res, next) {
@@ -13,7 +14,7 @@ router.get('/', function(req, res, next) {
 });
 
 /* GET data by ZIP */
-router.get("/:zip", async function(req, res) {
+router.get("/:zip", async function(req, res, next) {
   const sql = `SELECT * FROM enviro_data WHERE zip = ${req.params.zip}`;
 
   try {
@@ -29,5 +30,34 @@ router.get("/:zip", async function(req, res) {
     res.status(500).send({ error: error.message });
   }
 });
+
+/* GET data by city */
+router.get("/:zip/city", async function(req, res, next) {
+  // let cityName = req.params.city;
+
+  // const sql = `SELECT * FROM enviro_data WHERE city = ${cityName}`;
+
+  // try {
+  //   let result = await db(sql); // run the sql command: look for data by city
+  //   if (result.data.length === 0) {
+  //     // if the resulting array is empty, city doesn't exist: return error
+  //     res.status(404).send({ error: "City not found, please try again." });
+  //   } else {
+  //     res.status(200).send(result.data[0]); // send successful status and data to client
+  //   }
+  // } catch (error) {
+  //   // else send error if fails
+  //   res.status(500).send({ error: error.message });
+  // }
+
+ // POKEMON code to get attacks of Pokemon with spec'd ID
+  const pokemon = data.find(e => +e.id === +req.params.id) 
+  if (pokemon) {
+    res.send(pokemon.attacks); 
+  } else {
+    res.status(404).send("Pokemon does not exist");
+  }
+});
+
 
 module.exports = router;
