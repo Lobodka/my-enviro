6ef7b769cc5f68f7f4fc1865f81965fe70208063
Leo Salzmann <leo.salzmann@proton.me>
2022-11-08 11:55:45 +0100

    first styling commit

--- client/src/components/SelectCity.js
+++ client/src/components/SelectCity.js
@@ -38,38 +38,41 @@ function SelectCity(props) {
     }
 
     return(
-        <div className="bigDiv">
-            <h1>We found these ZIP codes for your city</h1>
+        <div className="BigDivEnergy">
+            <h1>Here's some data on your city</h1>
 
             <div className="cityStats">
                 {cityStats.map(stat => 
                 <p>
                    <span className="MinLevels">
-                   Minimum air pollution: {stat.min_air} <br/>
-                   Minimum amount of hazardous waste cleanup sites: {stat.min_haz_cleanups} <br/>
-                   Minimum likelihood of lead paint in houses: {stat.min_lead_paint} % <br/>
-                   Minimum water contamination level: {stat.min_water} % <br/>
+                   Minimum levels for... <br/>
+                   Air pollution: {stat.min_air} <br/>
+                   Amount of hazardous waste cleanup sites: {stat.min_haz_cleanups} <br/>
+                   Likelihood of lead paint in houses: {stat.min_lead_paint}% <br/>
+                   Water contamination: {stat.min_water}% <br/>
                    </span>
                   
                    <span className="MaxLevels">
-                   Maximum air pollution: {stat.max_air} <br/>
-                   Maximum amount of hazardous waste cleanup sites: {stat.max_haz_cleanups} <br/>
-                   Maximum likelihood of lead paint in houses: {stat.max_lead_paint} % <br/>
-                   Maximum water contamination level: {stat.max_water} % <br/>
+                   Maximum levels for... <br/>
+                   Air pollution: {stat.max_air} <br/>
+                   Amount of hazardous waste cleanup sites: {stat.max_haz_cleanups} <br/>
+                   Likelihood of lead paint in houses: {stat.max_lead_paint}% <br/>
+                   Water contamination level: {stat.max_water}% <br/>
                    </span>
 
                    <span className="AvgLevels">
-                   Average air pollution: {stat.avg_air} <br/>
-                   Average amount of hazardous waste cleanup sites: {stat.avg_haz_cleanups} <br/>
-                   Average likelihood of lead paint in houses: {stat.avg_lead_paint} % <br/>
-                   Average water contamination level: {stat.avg_water} % 
+                   Average levels for... <br/>
+                   Air pollution: {stat.avg_air} <br/>
+                   Amount of hazardous waste cleanup sites: {stat.avg_haz_cleanups} <br/>
+                   Likelihood of lead paint in houses: {stat.avg_lead_paint}% <br/>
+                   Water contamination level: {stat.avg_water}% 
                    </span>
                 </p>
                 )}
             </div>
            
            
-            <h2>Please select your ZIP code</h2>
+            <h2>Would you like to get more specific information? <br/> Please select your ZIP code from the ones listed below:</h2>
              <div className="zipButtons">
             {props.allZips.map(city => 
             <button className="chosenZip" onClick={e => handleClick(city.zip)} type="button">{city.zip}</button>)}
